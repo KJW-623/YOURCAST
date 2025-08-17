0575d73a497d4891bb5467335b7a69bef64bd850
KJW-623 <kimjiwoo4056@gmail.com>
2025-08-17 13:41:20 +0900

    이미지 수정

--- src/Intro/LogoIntro.js
+++ src/Intro/LogoIntro.js
@@ -4,7 +4,8 @@ import './LogoIntro.css';
 const LogoIntro = ({ fadingOut }) => {
     return (
         <div className={`intro-container ${fadingOut ? 'fade-out' : ''}`}>
-            <img src="${process.env.PUBLIC_URL}/yourcastLogo.png" alt="Logo" className="intro-logo" />
+            <img src={`${process.env.PUBLIC_URL}/yourcastLogo.png`} alt="Logo" className="intro-logo"
+/>
         </div>
     );
 };
